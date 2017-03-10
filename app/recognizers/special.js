6ae321a2aff1c1c1ebd7346463b4f1aeb8e2757e
John Alioto <johnali@microsoft.com>
2017-03-10 12:58:01 -0800

    clean up special handler for codes

--- app/recognizers/special.js
+++ app/recognizers/special.js
@@ -1,22 +1,24 @@
-const codes = [
-    '2bebe5f5-027c-4401-aaf6-c323a8a4c684',
-    '428199ac-eecc-4921-b7f8-0578d7e9e1e5'
-];
+const _ = require('lodash')
 
 module.exports = {
+    getCodes: function()  {
+        var codes = ['2bebe5f5-027c-4401-aaf6-c323a8a4c684', '428199ac-eecc-4921-b7f8-0578d7e9e1e5'];
+        var diags = ['menu', 'recipe'];
+
+        return _.zipWith(codes, diags, (c,d) => { return { code: c, dialog: d}; } ); 
+    },
     recognize: function(context, callback) {
-
         var t = context.message.text;
-        var isCode = codes.some(g => t === g);
+        var isCode = this.getCodes().some(c => c.code === t);
 
-        var recognized = {
+        var result = {
             entities: [],
-            intent: (isCode ? 'code' : null),
+            intent: (isCode ? 'special' : null),
             matched: undefined,
             expression: undefined,
             intents: [],
             score: (isCode ? 1 : 0)
         }
-        callback.call(null, null, recognized);
+        callback.call(null, null, result);
     }
 };
