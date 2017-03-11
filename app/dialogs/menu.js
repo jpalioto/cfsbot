d610e8f329afe642caf4418c562dac644b4dd006
John Alioto <johnali@microsoft.com>
2017-03-10 23:33:51 -0800

    flesh out menu dialog

--- app/dialogs/menu.js
+++ app/dialogs/menu.js
@@ -1,23 +1,38 @@
 const builder = require('botbuilder');
 const cfsApi  = require('../utilities/apihandler.js');
 
+const menuPrompts = ['What menu item would you like ingredients for?']
+
 module.exports = function (bot) {
     bot.dialog('/menuDialog', 
     [
-        s => 
+        (s,r,n) => 
         { 
             var menu = cfsApi.callApi('menu');
+            var  mealItems = [];
 
             menu.then( res => {
-
+                
                 res.forEach( e => {
-                    e.MealItems.forEach( mi => {
-                        s.send(mi.Name);
-                    })
-                } );
+                    
+                    e.MealItems.forEach( mi => mealItems.push(mi.Name) );
 
-                s.endDialog();
+                });
+
+                builder.Prompts.choice(s, 
+                    'Which menu item would you like ingredients for?', 
+                    mealItems.join('|'),
+                    {
+                        listStyle: builder.ListStyle.list
+                    });
             });
+        },
+        (s,r,n) =>
+        {
+            // TODO: Get menu items for choice
+            var choice = r.response.entity;
+            s.send('You chose ' + choice);
+            s.endDialog();
         }    
     ]);
 };
